9f69e3d89deccc4a6e83660cffb4f34b52c68c98
Shameek Sarkar <sham3k@gmail.com>
2018-01-15 14:20:58 +0530

    Initial Commit

---
+++ Heisenberg/Search/FirebaseContactManager.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseContactManager.swift
+//  Heisenberg
+//
+//  Created by Shameek Sarkar on 15/01/18.
+//  Copyright © 2018 Shameek. All rights reserved.
+//
+
+import Foundation
---
+++ Heisenberg/Search/SearchResultCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResultCell.swift
+//  Heisenberg
+//
+//  Created by Shameek Sarkar on 14/01/18.
+//  Copyright © 2018 Shameek. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
